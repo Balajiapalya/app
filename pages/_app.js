f169f20f7894e1eb1571a9a2f6df1207f9a6ea7f
balajiapalya <rbalaji@apalya.com>
2022-02-18 17:09:11 +0530

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,22 @@
 import '../styles/globals.css'
-import Layout from './layout'
+// import Layout from './layout'
 
+// function MyApp({ Component, pageProps }) {
+
+//   // return  (
+//     // <div>
+//       {/* <Layout> */}
+//       return  <Component {...pageProps} />
+//     //   </Layout>
+//     // </div>
+
+//   // );  
+// } 
 function MyApp({ Component, pageProps }) {
+  // Use the layout defined at the page level, if available
+  const getLayout = Component.getLayout || ((page) => page)
 
-  return (
-    <div>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </div>
-
-  );  
+  return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
--- pages/layout.js
+++ pages/layout.js
@@ -1,6 +1,6 @@
 
 import Navbar from '../components/common/navbar';
-import Settings from './settings';
+
 
 const Layout = ({ children }) => {
     return (
