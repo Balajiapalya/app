b91a84bb6967b9c18a59412f1ffb7e00c3be783a
saipraneethv <saipraneethv@yupptv.com>
2022-04-01 08:58:23 +0530

    updated biiling
    edit payment details , broken link fixed

--- app/components/homepage/settingstabs/billing.js
+++ app/components/homepage/settingstabs/billing.js
@@ -18,11 +18,10 @@ function Billing() {
         Api.Get_account_info()
             .then(res => {
                 if (res.data.status = "Success") {
+                    seturl(res.data.data.changeBillingUrl)
                     if(res&&res.data&&res.data.data&&res.data.data.billingInfo){
-                        set_accDetails(res.data.data.billingInfo)
-                        seturl(res.data.data.changeBillingUrl)
-                    }
-                    
+                        set_accDetails(res.data.data.billingInfo)  
+                    }    
                 }
             })
             .catch(error => {
