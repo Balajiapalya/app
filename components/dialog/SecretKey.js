492dc9de15f4f2b67228dd08b2395df4bb2f5da0
farheen fatima <farheen.f@apalya.com>
2022-04-19 15:47:53 +0530

    fixed error

--- components/dialog/SecretKey.js
+++ components/dialog/SecretKey.js
@@ -11,12 +11,12 @@ const [accessKey,setAccessKey]=useState()
             <div className={styles.model_nav}>
                <a className={styles.model_close} role="button" onClick={() => setNewToken(false)}><img src="Images/close.png" alt='icon' /> </a>
             </div>
-                <h2>Here's your new Access Token:</h2>
+                <h2>Here&apos;s your new Access Token:</h2>
                 <h3>Access Token ID:</h3>
                 <input />
                 <img className={styles.imgCopy} src="Images/favicon/copy.png"/>
                 <h3>Secret Key:</h3>
-                <h4>We don't store this so please memorize it...</h4>
+                <h4>We don&apos;t store this so please memorize it...</h4>
                 <input />
                 <img className={styles.imgCopy} src="Images/favicon/copy.png"/>
                 <br/>
