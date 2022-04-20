7b2e89532b024b94b6e80e8463ad34705e5a1e3b
saipraneethv <saipraneethv@yupptv.com>
2022-04-20 16:15:48 +0530

    recent commit

--- components/analytics_tabs/metrics.js
+++ components/analytics_tabs/metrics.js
@@ -1,24 +1,196 @@
-import styles from '../../styles/analytics_tabs.module.css'
-export default function Metrics(){
-    return(
+import { useEffect, useState } from 'react';
+import styles from '../../styles/analytics_tabs.module.css';
+import Api from '../api/api';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export default function Metrics() {
+    const [viewsStatistics, set_viewsStatistics] = useState([]);
+    const [videoviews, setvideoviews] = useState([]);
+    const [countryviews, setcountryviews] = useState([]);
+    const [divicesviews, setdeviceviews] = useState([]);
+    const [playerviews, setplayerviews] = useState([]);
+    const [applicationsviews, setapplicationsviews] = useState([]);
+    useEffect(() => {
+        Views_statistics_data()
+    }, [])
+    const Views_statistics_data = () => {
+        Api.Views_statistics()
+            .then(res => {
+                set_viewsStatistics(res.data.data);
+                setvideoviews(res.data.data.videoViews);
+                setcountryviews(res.data.data.countryViews);
+                setdeviceviews(res.data.data.deviceViews);
+                setplayerviews(res.data.data.playerViews);
+                setapplicationsviews(res.data.data.applicationViews)
+            })
+    }
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: false,
+                position: 'bottom',
+            },
+            title: {
+                position: 'bottom',
+                display: false,
+                text: 'Chart.js Bar Chart',
+            },
+        },
+    };
+    // const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+    const videos_data = {
+        labels: videoviews.map((videos, key) => videos?.key),
+        datasets: [
+            {
+
+                data: videoviews.map((item, key) => item?.percentage),
+                backgroundColor: 'rgba(53, 162, 235, 1)',
+            },
+        ],
+    };
+    const device_data = {
+        labels: divicesviews.map((videos, key) => videos?.key),
+        datasets: [
+            {
+                data: divicesviews.map((item, key) => item?.percentage),
+                backgroundColor: 'rgba(53, 162, 235, 1)',
+            },
+        ],
+    }
+    const player_data = {
+        labels: playerviews.map((player, key) => player?.count),
+        datasets: [
+            {
+                data: playerviews.map((item, key) => item?.percentage),
+                backgroundColor: 'rgba(53, 162, 235, 1)',
+            },
+        ],
+    }
+    const application_data = {
+        labels: applicationsviews.map((application, key) => application?.key),
+        datasets: [
+            {
+                data: applicationsviews.map((item, key) => item?.percentage),
+                backgroundColor: 'rgba(53, 162, 235, 1)',
+            },
+        ],
+    }
+    return (
         <div className={styles.container}>
             <div className={styles.Metrics_heading}>
                 <h3>Viewership Insights</h3>
-                <div>       
+                <div>
                     <div></div>
                     <div></div>
                 </div>
             </div>
-            <div className={styles.views_cards}>
-                <div className={styles.cards_container}></div>
-                <div className={styles.cards_container}></div>
-                <div className={styles.cards_container}></div>
-                <div className={styles.cards_container}></div>
-            </div>
-            <div className={styles.Countries_videos}>
-                <div className={styles.countries}></div>
-                <div className={styles.videos}></div>
-            </div>
+            {[viewsStatistics].map((item, key) => <>
+
+                <div className={styles.views_cards}>
+
+                    <div className={styles.cards_container}>
+                        <h4>No. of Views</h4>
+                        <div className={styles.totalViews}>
+                            <h5>{item.totalViews}</h5>
+                            <span>Number of views that <br></br> started playback</span>
+                        </div>
+                    </div>
+                    <div className={styles.cards_container}>
+                        <h4>No. of Unique Views</h4>
+                        <div className={styles.UniqueViews}>
+                            <h5>{item.uniqueViews}</h5>
+                            <span>Unique viewers that started<br /> playback,based on User ID.</span>
+                        </div>
+                    </div>
+                    <div className={styles.cards_container}>
+                        <h4>Watched Time</h4>
+                    </div>
+                    <div className={styles.cards_container}>
+                        <h4>Errors</h4>
+                    </div>
+                </div>
+                <div className={styles.Countries_videos}>
+                    <div className={styles.countries_container}>
+                        <div className={styles.countries_heading}>
+                            <h4 className={styles.heading}>Countries</h4>
+                            <span>Viewership in the last 7 days.</span>
+                        </div>
+                        <div className={styles.countries_map} ></div>
+                        <div className={styles.countries_table} >
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th >Country</th>
+                                        <th>Percentage</th>
+                                        <th>Views</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {countryviews.map((country, key) =>
+                                        <tr key={key}>
+                                            <td className={styles.countries_name}>{country.key}</td>
+                                            <td>{country.percentage}%</td>
+                                            <td>{country.count}</td>
+                                        </tr>
+                                    )}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <div className={styles.metric_cards}>
+                        <h4>
+                            Videos
+                        </h4>
+                        <span>Viewership in the last 7 days.</span>
+                        <div>
+                            <Bar options={options} data={videos_data} />
+                        </div>
+
+                    </div>
+                </div>
+                <div className={styles.graphs_container}>
+                    <div className={styles.devices_card}>
+                        <h4>Devices</h4>
+                        <span>Viewership in the last 7 days.</span>
+                        <div>
+                            <Bar options={options} data={device_data} />
+                        </div>
+                    </div>
+                    <div className={styles.player_card}>
+                        <h4>Player</h4>
+                        <span>Viewership in the last 7 days.</span>
+                        <div>
+                            <Bar options={options} data={player_data} />
+                        </div>
+                    </div>
+                    <div className={styles.applications_card}>
+                        <h4>Applications</h4>
+                        <span>Viewership in the last 7 days.</span>
+                        <div>
+                            <Bar options={options} data={application_data} />
+                        </div>
+                    </div>
+                </div>
+            </>)}
         </div>
     )
 }
--- components/analytics_tabs/overview.js
+++ components/analytics_tabs/overview.js
@@ -9,21 +9,21 @@ ChartJS.register(ArcElement, Tooltip, Legend);
 export default function Overview() {
     const [usagestatistics, set_usagestatistics] = useState([]);
     const [viewsStatistics, set_viewsStatistics] = useState([]);
-    const [deviceviews, setdeviceviews] = useState([])
-    const [countryviews, setcountryviews] = useState([])
+    const [deviceviews, setdeviceviews] = useState([]);
+    const [countryviews, setcountryviews] = useState([]);
     useEffect(() => {
         Usage_statistics_data();
         Views_statistics_data();
-    }, [])
+    }, []);
     const Usage_statistics_data = () => {
         Api.Usage_statistics()
             .then(res => {
-                // console.log(res.data.data.totalUsageRecords)
+                // console.log(res.data.data)
                 set_usagestatistics(res.data.data.totalUsageRecords)
 
 
             })
-    }
+    };
     const Views_statistics_data = () => {
         Api.Views_statistics()
             .then(res => {
@@ -33,7 +33,16 @@ export default function Overview() {
                 setcountryviews(res.data.data.countryViews)
                 // console.log(res.data.data.countryViews)
             })
-    }
+    };
+    const options = {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'bottom',
+            },
+        },
+    };
     const doughnutdata = {
         labels: deviceviews.map((device, key) => device?.key),
         datasets: [
@@ -67,27 +76,36 @@ export default function Overview() {
                     <div key={key} className={styles.encoded_video}>
                         <h5 className={styles.video_type_heading}>{item.usage}</h5>
                         <div className={styles.video_type_content}>
-                            <h5>{item.amountInSecs}</h5>
-                            <p className={styles.timeperiod}>Total minutes of videos encoded in last 7 days.</p>
+                            <h5>{item.amountInSecs} Secs</h5>
+                            <div className={styles.timeperiod}>
+                                <span >Total minutes of videos <br></br>encoded in last 7 days.</span>
+                            </div>
                         </div>
-
                     </div>)}
             </div>
             {[viewsStatistics].map((items, keys) => <>
                 <div className={styles.real_time_views_container}>
                     <h4 className={styles.heading}>Real-Time Views</h4>
-                    <h5>{items.totalViews}</h5>
+                    <div className={styles.viewers_details}>
+                        <h5 className={styles.totalViews}>{items.totalViews}</h5>
+                        <span className={styles.watching_viewers}>users are watching content right now.</span>
+                    </div>
+
 
                 </div>
                 <div className={styles.countries_devices_container}>
                     <div className={styles.countries_container}>
-                        <h4 className={styles.heading}>Countries</h4>
+                        <div className={styles.countries_heading}>
+                            <h4 className={styles.heading}>Countries</h4>
+                            <span>Viewership in the last 7 days.</span>
+                        </div>
+
                         <div className={styles.countries_map} ></div>
                         <div className={styles.countries_table} >
                             <table>
                                 <thead>
                                     <tr>
-                                        <th>Country</th>
+                                        <th >Country</th>
                                         <th>Percentage</th>
                                         <th>Views</th>
                                     </tr>
@@ -97,8 +115,8 @@ export default function Overview() {
                                 <tbody>
                                     {countryviews.map((country, key) =>
                                         <tr key={key}>
-                                            <td>{country.key}</td>
-                                            <td>{country.percentage}</td>
+                                            <td className={styles.countries_name}>{country.key}</td>
+                                            <td>{country.percentage}%</td>
                                             <td>{country.count}</td>
                                         </tr>
                                     )}
@@ -106,11 +124,17 @@ export default function Overview() {
 
                             </table>
                         </div>
+                        <div className={styles.more_insights}>
+                             <a>More Insights&gt;</a>
+                        </div>
                     </div>
                     <div className={styles.devices_container}>
                         <h4 className={styles.heading}>Devices</h4>
                         <div className={styles.doughnut_graph}>
-                            <Doughnut data={doughnutdata} />
+                            <Doughnut options={options} data={doughnutdata} />
+                        </div>
+                        <div className={styles.more_insights}>
+                             {/* <a>More Insights&gt;</a> */}
                         </div>
                     </div>
                 </div>
