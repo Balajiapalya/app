8a697519c1d61ebcebc5aaa24a82df7e22f1c656
farheen fatima <farheen.f@apalya.com>
2022-05-04 18:09:18 +0530

    changes done

--- components/homepage/settingstabs/Select.js
+++ components/homepage/settingstabs/Select.js
@@ -1,29 +1,55 @@
 import React, { useState } from 'react'
 import Api from '../../api/api'
+import styles from '../../../styles/settings.module.css';
 
 const Select = ({ item, data }) => {
+    const [isActive,setIsActive]=useState(false)
+    const [selected,setSelected]=useState([])
     const [opt, setOpt] = useState(item.roleId)
+    
+    // const handleChange = (e) => {
+    //     setOpt(e.target.value)
+    //     let element = new Object()
+    //     element.email = item.email
+    //     element.roleId = parseInt(e.target.value)
+    //     let arr = []
+    //     arr.push(element)
+    //     Api.Selected_option(arr).then(res => console.log(res))
+    // }
 
-    const handleChange = (e) => {
 
-        setOpt(e.target.value)
 
+    const handleChange = (i) => {
+        setOpt(i.name)
         let element = new Object()
         element.email = item.email
-        element.roleId = parseInt(e.target.value)
+        element.roleId = parseInt(i.id)
         let arr = []
         arr.push(element)
-        // let a=JSON.stringify(element)
         Api.Selected_option(arr).then(res => console.log(res))
+        setIsActive(false)
     }
+
     return (
-        <>
-            <select onChange={(e) => handleChange(e)}>
-                {data.map(i => <>
-                    <option selected={opt == i.id} value={i.id}>{i.name}</option>
-                </>)}
-            </select>
-        </>
+        // <div>
+        //     <select onChange={(e) => handleChange(e)}>
+        //         {data.map(i => <>
+        //             <option selected={opt == i.id} value={i.id}>{i.name}</option>
+        //         </>)}
+        //     </select>
+        // </div>
+
+        <div className={styles.dropdown}>
+        <div className={styles.dropdownBtn} onClick={()=>setIsActive(!isActive)}>Owner</div>
+        
+        {isActive && <div className={styles.dropdownContent}>
+            {data.map(i => <div>
+                <div onClick={()=>handleChange(i)} className={styles.dropdownItem}>{i.name}</div>
+            </div>)}
+        </div>}
+    </div>
+
+
     )
 }
 export default Select
