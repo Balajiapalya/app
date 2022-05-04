d81bb3802e94ad9fe6dd8d7df0efb60b1c88da0e
saipraneethv <saipraneethv@yupptv.com>
2022-05-04 15:30:59 +0530

    recent commit

--- pages/environments/index.js
+++ pages/environments/index.js
@@ -179,15 +179,15 @@ export default function Environment() {
                           <span className={styles.box_content_history}>
                             in last 7 days
                           </span>
-                          {[OrgStats[items.uuid]].map(orgStat =>
-                            <div  className={styles.box_data}>
+                          {[OrgStats].map((item, key) =>
+                            <div key={key} className={styles.box_data}>
                               <div className={styles.box_data_types}>
                                 <span className={styles.types_heading}>
                                   Encoded
                                 </span>
                                 <br />
                                 <span className={styles.types_value}>
-                                  {(orgStat && Math.round((orgStat.RecordEncodingUsage || 0)/(60*60))) || 0} hours
+                                  {item.RecordEncodingUsage}
                                 </span>
                               </div>
                               <div className={styles.box_data_types}>
@@ -196,7 +196,7 @@ export default function Environment() {
                                 </span>
                                 <br />
                                 <span className={styles.types_value}>
-                                  {(orgStat && Math.round((orgStat.RecordStorageUsage || 0)/(60*60))) || 0} hours
+                                {item.RecordStorageUsage}
                                 </span>
                               </div>
                               <div className={styles.box_data_types}>
@@ -205,11 +205,13 @@ export default function Environment() {
                                 </span>
                                 <br />
                                 <span className={styles.types_value}>
-                                  {(orgStat && Math.round((orgStat.RecordStreamingUsage || 0)/(60*60))) || 0} hours
+                                {item.RecordStreamingUsage}
+                                {item.environmentKey}
                                 </span>
                               </div>
                             </div>
                           )}
+
                           <div>
                             <span className={styles.token_key_value}>
                               API tokens: {items.accessTokensCount}
@@ -225,7 +227,6 @@ export default function Environment() {
                           <span className={styles.box_content_history}>
                             in last 7 days
                           </span>
-                          {[OrgStats[items.uuid]].map(orgStat =>
                           <div className={styles.box_data}>
                             <div className={styles.box_data_types}>
                               <span className={styles.types_heading}>
@@ -233,7 +234,7 @@ export default function Environment() {
                               </span>
                               <br />
                               <span className={styles.types_value}>
-                                {(orgStat && (orgStat.TotalViews || 0)) || 0}
+                                {/* 20 */}
                               </span>
                             </div>
                             <div className={styles.box_data_types}>
@@ -242,7 +243,7 @@ export default function Environment() {
                               </span>
                               <br />
                               <span className={styles.types_value}>
-                                {(orgStat && (orgStat.UniqueViews || 0)) || 0}
+                                {/* 15 */}
                               </span>
                             </div>
                             <div className={styles.box_data_types}>
@@ -251,11 +252,10 @@ export default function Environment() {
                               </span>
                               <br />
                               <span className={styles.types_value}>
-                                {(orgStat && Math.round((orgStat.RecordStreamingUsage || 0)/(60*60))) || 0} hours
+                                {/* 20 mins */}
                               </span>
                             </div>
                           </div>
-                          )}
                           <div>
                             <span className={styles.token_key_value}>
                               Env Key: {items.environmentKey}
