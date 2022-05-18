d7aeb616127b8f325bcbdeba319603b21b64ac5f
saipraneethv <saipraneethv@yupptv.com>
2022-05-18 13:06:55 +0530

    recent commit

--- components/dialog/removeuser.js
+++ components/dialog/removeuser.js
@@ -4,29 +4,31 @@ import Api from '../api/api'
 import { useState } from 'react/cjs/react.production.min'
 
 
-export default function Removeuser({ closeremoveuser ,item }) {
-  
-  const handleDelete=()=>{
-    let del=[item.email]
-    Api.Remove_user_data(del).then(res=>{
+export default function Removeuser({ closeremoveuser, item }) {
+
+  const handleDelete = () => {
+    let del = [item.email]
+    Api.Remove_user_data(del).then(res => {
       closeremoveuser(false)
     })
   }
   return (
     <div className={`${styles.model} ${styles.remove_user_modal}`} >
-      <div className={styles.model_main}>
-        <div className={styles.model_nav}>
-          <h3 className={styles.model_title}>Remove User</h3>
-          <a className={styles.model_close} onClick={() => closeremoveuser(false)}><img src="images/close.png" alt='icon'/> </a>
-        </div>
+      <div className={styles.model_container}>
+        <div className={styles.model_main}>
+          <div className={styles.model_nav}>
+            <h3 className={styles.model_title}>Remove User</h3>
+            <a className={styles.model_close} onClick={() => closeremoveuser(false)}><img src="images/close.png" alt='icon' /> </a>
+          </div>
 
-        <div className={styles.model_removeuser}>
-          <h4 className={styles.model_removeusername}>Are you sure you want to remove user?</h4>
-          <p className={styles.model_data}>Once removed,this user will no longer be able to access this organisation on Videograph.</p>
-        </div>
-        <div className={styles.model_btn}>
-          <a><button type="button" className={`${styles.model_canel_btn} btn btn-primary`} onClick={() => closeremoveuser(false)}>Cancel</button></a>
-          <a><button type="button" onClick={()=>handleDelete()} className={`${styles.model_save_btn} btn btn-primary`} >Yes, remove</button></a>
+          <div className={styles.model_removeuser}>
+            <h4 className={styles.model_removeusername}>Are you sure you want to remove user?</h4>
+            <p className={styles.model_data}>Once removed,this user will no longer be able to access this organisation on Videograph.</p>
+          </div>
+          <div className={styles.model_btn}>
+            <a><button type="button" className={`${styles.model_canel_btn} btn btn-primary`} onClick={() => closeremoveuser(false)}>Cancel</button></a>
+            <a><button type="button" onClick={() => handleDelete()} className={`${styles.model_save_btn} btn btn-primary`} >Yes, remove</button></a>
+          </div>
         </div>
       </div>
     </div>
