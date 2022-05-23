f5ca53548ab5c2a705e71ef3c3bf78471be6dc73
N T R Phani Kumar <ntrphanikumar@yupptv.com>
2022-05-23 17:40:49 +0530

    Renamed to new name

---
+++ marketing/app1/pages/index.js
@@ -0,0 +1,45 @@
+
+import styles from '../styles/Home.module.css'
+import Lets_talk from '../components/lets_talk'
+import { useState } from 'react'
+
+export default function Home() {
+  const [openmodel,setmodel]=useState(false);
+  return (
+    <div className={styles.container_body}>
+      <div className={styles.container}>
+        <div className={styles.content_header}>
+          <div className={styles.header_logo}>
+            <img className={styles.logo} src="./logo.svg" alt="logo" />
+          </div>
+          <div className={styles.header_btns}>
+            <button>API Docs</button>
+            <button className={styles.sign_in_btn}><a>Sign in</a></button>
+            <button className={styles.sign_up_btn}><a>Sign up</a></button>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.body_content}>
+            <h2>Plug & Play APIs for Live &
+              On-demand video streaming.
+            </h2>
+            <h4>
+              Manage entire video life cycle from encoding, storage and streaming using developer friendly
+              APIs.
+            </h4>
+            <div className={styles.content_btn}>
+              <button className={styles.btn_get_started_free}>Get started free</button>
+              <button onClick={()=>setmodel(true)} className={styles.btn_talk_to_us}>Talk to us</button>
+            </div>
+          </div>
+          <div className={styles.content_img}>
+            <img src="./design.svg" alt="design" />
+          </div>
+        </div>
+
+
+      </div>
+      {openmodel&&<Lets_talk closemodel={setmodel}/> }
+    </div>
+  )
+}
