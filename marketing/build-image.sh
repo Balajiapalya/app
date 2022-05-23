3a52d19b0b0f4737d27b03f569f6db214940f8ee
N T R Phani Kumar <ntrphanikumar@yupptv.com>
2022-05-23 15:18:56 +0530

    Added marketing app also to project

---
+++ marketing/build-image.sh
@@ -0,0 +1,7 @@
+test=`git branch | grep "*"`
+currentBranch=${test:2}
+
+aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 989431465196.dkr.ecr.ap-south-1.amazonaws.com
+
+docker build -t ntrphanikumaryupptv/yupptv:vg-marketing-$currentBranch -t 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-marketing:$currentBranch .
+docker push 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-marketing:$currentBranch
