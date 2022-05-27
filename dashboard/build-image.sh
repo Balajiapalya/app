686a6a03408f2c0ce914c27b96a18dfe43afd701
N T R Phani Kumar <ntrphanikumar@yupptv.com>
2022-05-27 10:53:25 +0530

    Added versioning

--- dashboard/build-image.sh
+++ dashboard/build-image.sh
@@ -1,7 +1,10 @@
-test=`git branch | grep "*"`
-currentBranch=${test:2}
+#test=`git branch | grep "*"`
+#currentBranch=${test:2}
+
+release_version=`cat version.txt`
+build_version=`cat version.txt`.$(date '+%Y%m%d')
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 989431465196.dkr.ecr.ap-south-1.amazonaws.com
 
-docker build -t ntrphanikumaryupptv/yupptv:vg-dashboard-$currentBranch -t 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-dashboard:$currentBranch .
-docker push 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-dashboard:$currentBranch
+docker build -t ntrphanikumaryupptv/yupptv:vg-dashboard-$release_version -t 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-dashboard:$build_version .
+docker push 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-dashboard --all-tags
--- marketing/build-image.sh
+++ marketing/build-image.sh
@@ -1,7 +1,10 @@
-test=`git branch | grep "*"`
-currentBranch=${test:2}
+#test=`git branch | grep "*"`
+#currentBranch=${test:2}
+
+release_version=`cat version.txt`
+build_version=`cat version.txt`.$(date '+%Y%m%d')
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 989431465196.dkr.ecr.ap-south-1.amazonaws.com
 
-docker build -t ntrphanikumaryupptv/yupptv:vg-marketing-$currentBranch -t 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-marketing:$currentBranch .
-docker push 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-marketing:$currentBranch
+docker build -t 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-marketing:$release_version -t 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-marketing:$build_version .
+docker push 989431465196.dkr.ecr.ap-south-1.amazonaws.com/vg-marketing --all-tags
