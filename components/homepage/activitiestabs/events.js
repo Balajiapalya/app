ee5948e188da55f7976336387cde44ac42adac99
saipraneethv <saipraneethv@yupptv.com>
2022-07-01 12:27:16 +0530

    recent changes in videos/overview/activities/events and logs

--- components/homepage/activitiestabs/events.js
+++ components/homepage/activitiestabs/events.js
@@ -13,7 +13,7 @@ function Events() {
   const getEventData = () => {
     Api.Get_Activities_Events(query)
       .then(res => {
-        if (res && res.data && res.data.data) {
+        if (res && res.data && res.data.data && res.data.data.length>0) {
           setactivitiesData(res.data.data)
         }
       })
@@ -29,42 +29,25 @@ function Events() {
 
     <div className={styles.activities_events}>
       <p className={styles.activites_details}>Events let you know when something ha happened in your system.<br></br>Events will be stored up to 30 days.</p>
-      <table className={styles.activities_events_logs_table}>
+      {activitiesData !== "" ?<table className={styles.activities_events_logs_table}>
         <thead>
           <tr>
             <th>EVENTS</th>
-            <th >ID</th>
+            <th  className={styles.Id_head}>ID</th>
             <th>Date</th>
           </tr>
         </thead>
         <tbody>
-          {activitiesData.flatMap((data, key) =>
+          {activitiesData.map((data, key) =>
             <tr key={key}>
               <td>{data.messageType}</td>
               <td className={styles.Id}>{data.uuid}</td>
               <td>{createdOn(data.occurredOn)}</td>
             </tr>
           )}
-
-          {/* <tr>
-            <td>Video.detect.framefreeze</td>
-            <td>abae8ba0-0970-41a6-834c-667110b861031</td>
-            <td>11/11/2021 3:57PM</td>
-          </tr>
-          <tr>
-            <td>Video.detect.framefreeze</td>
-            <td>abae8ba0-0970-41a6-834c-667110b861031</td>
-            <td>11/11/2021 3:57PM</td>
-          </tr> */}
         </tbody>
-      </table>
-
-
-
-
+      </table>:<p className={styles.data_not_found}>No data found</p>}
     </div>
-
-
   )
 }
 export default Events;
--- components/homepage/activitiestabs/logs.js
+++ components/homepage/activitiestabs/logs.js
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Api from "../../api/api";
 import { useRouter } from "next/router";
 function Logs() {
+  const [LogsData, setLogsData] = useState([]);
   const router = useRouter();
   const query = router.query.videoId
   useEffect(() => {
@@ -12,13 +13,16 @@ function Logs() {
   const getEventData = () => {
     Api.Get_Activities_Logs(query)
       .then(res => {
-        if (res && res.data && res.data.data) {
-          console.Log(res.data.data)
-        }
+        if(res && res.data && res.data.data && res.data.data.length>0)
+        setLogsData(res.data.data)
       })
       .catch(error => {
         console.log(error)
       })
+  }
+  const createdOn = (date) =>{
+    var dateNew = new Date(+date).toLocaleString('en-In',{day:"2-digit",month:'2-digit',year:'numeric',hour:'2-digit',minute:'2-digit',second:'2-digit'})
+    return dateNew
   }
   return (
 
@@ -27,32 +31,24 @@ function Logs() {
 
       <p className={styles.activites_details}>Logs show every API action taken.Logs will be stored up to 30 days.</p>
 
-      <table className={styles.activities_events_logs_table}>
-        <thead>
-          <tr>
-            <th>Status</th>
-            <th>Description</th>
-            <th>Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Video.detect.framefreeze</td>
-            <td>abae8ba0-0970-41a6-834c-667110b861031</td>
-            <td>11/11/2021 3:57PM</td>
-          </tr>
-          <tr>
-            <td>Video.detect.framefreeze</td>
-            <td>abae8ba0-0970-41a6-834c-667110b861031</td>
-            <td>11/11/2021 3:57PM</td>
-          </tr>
-          <tr>
-            <td>Video.detect.framefreeze</td>
-            <td>abae8ba0-0970-41a6-834c-667110b861031</td>
-            <td>11/11/2021 3:57PM</td>
-          </tr>
-        </tbody>
-      </table>
+      {LogsData !== "" ?
+        <table className={styles.activities_events_logs_table}>
+          <thead>
+            <tr>
+              <th className={styles.status}>Status</th>
+              <th>Description</th>
+              <th className={styles.date_header}>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {LogsData.map((logs, key) =>
+              <tr key={key}>
+                <td className={styles.status}>{logs.status}</td>
+                <td className={styles.description_content}><span>{logs.url}</span><br/><span>{logs.uuid}</span></td>
+                <td>{createdOn(logs.occurredOn)}</td>
+              </tr>)}
+          </tbody>
+        </table> : <p className={styles.data_not_found}>No data found</p>}
 
     </div>
 
