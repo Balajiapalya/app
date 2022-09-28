2bb6c239bd7eb5f4dd79e0458fe17b02b813b265
saipraneethv <saipraneethv@yupptv.com>
2022-09-28 13:08:37 +0530

    changes in iframe and bug fixes
    fixed bugs: 96,99,100
    small changes in iframe.

--- dashboard/app/pages/email.js
+++ dashboard/app/pages/email.js
@@ -3,12 +3,12 @@ import { useForm } from "react-hook-form";
 import Api from '../components/api/api';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-
+import Link from 'next/link';
 
 
 export default function Signin() {
     const router = useRouter()
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit, watch, setError,formState: { errors } } = useForm();
     const [msg, setmsg] = useState(false);
 
     const onSubmit = (data) => {
@@ -16,12 +16,15 @@ export default function Signin() {
             .then(res => {
                 if (res.data.status = "Success") {
                     setmsg(res.data.message)
+                    setTimeout(() => {
+                        setmsg('')
+                    }, 2000);
+                  
                 }
 
             })
             .catch(error => {
-                setmsg(error.response.data.message)
-
+                setError('email',{message:error.response.data.message})
             })
 
 
@@ -34,24 +37,31 @@ export default function Signin() {
                 <img src='/images/logo.svg' alt='logo' />
                 <div className={styles.signup_area}>
                     <form onSubmit={handleSubmit(onSubmit)}>
-                        <label className={styles.label}><h3>Enter your email address:</h3></label>
+                        <label className={styles.label}>Please Enter Your Registered Email:</label>
                         <input
                             maxLength={40}
-                            type="email"
                             placeholder="Enter your email address"
                             name="email"
                             className={`${styles.signup_input} ${styles.inp} form_control`}
                             {...register("email",{required:'This field is required',pattern:{
                                 value:/^[A-Z0-9_%+-.^*!$`~]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                message:'invalid email address'
+                                message:'Invalid Email Address'
                               }})}
                         />
                          {<p className={'validations'}>{errors.email?.message}</p>}
-                         <span className='error'>{msg}</span>
-                        
+                         <div className={styles.center}>
+                         <span>{msg}</span>
                         <button type='submit' className={`${styles.signup_btn} btn btn-primary`}>Submit</button>
+                        </div>
                     </form>
-
+                    <div className={styles.forgotRemember}>
+                    <h4>Don&#39;t have an account?</h4>
+                    <Link href="/signup"><a className={styles.forgotLinks}>Sign up</a></Link>
+                    </div>
+                    <div  className={styles.forgotRemember}>
+                    <h4>Already have an account?</h4>
+                    <Link href="/"><a className={styles.forgotLinks}>Sign In</a></Link>
+                    </div>
                 </div>
             </main>
         </div>
